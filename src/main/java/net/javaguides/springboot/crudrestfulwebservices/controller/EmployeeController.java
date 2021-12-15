83607aaf057a1869149eccb5c49f1826623a23b3
krishnamanoj.d <krishnamanojdevarakonda@gmail.com>
2021-12-15 20:01:38 +0530

    Creating the project

---
+++ src/main/java/net/javaguides/springboot/crudrestfulwebservices/controller/EmployeeController.java
@@ -0,0 +1,67 @@
+package net.javaguides.springboot.crudrestfulwebservices.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.crudrestfulwebservices.exception.ResourceNotFoundException;
+import net.javaguides.springboot.crudrestfulwebservices.model.Employee;
+import net.javaguides.springboot.crudrestfulwebservices.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//create get all employees api
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	// create employee
+	@PostMapping("/employees")  //@RequestBody annotation is used to convert multiples inputs from Http request to class variable (i.e. employee) 
+	public Employee createEmployee(@RequestBody Employee employee) {
+		
+		return employeeRepository.save(employee);
+	}
+	//get employee by id
+	@GetMapping("/employees/{id}")  //@PathVariable annotation is used to bind the HTTP template variable (i.e. "id" here) to the method parameter (i.e. employeeId here)
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value="id") long employeeId) throws ResourceNotFoundException{
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for the given id"));
+		return ResponseEntity.ok().body(employee);
+	}
+	
+	// update employee
+	@PutMapping("employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value="id") long employeeId, @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+		Employee employee=employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setFirstName(employeeDetails.getLastName());
+		employee.setFirstName(employeeDetails.getEmailId());
+		employeeRepository.save(employee);
+		return ResponseEntity.ok().body(employee);
+		
+		
+	}
+	// delete employee by id
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<?> deleteEmployee(@PathVariable(value="id") long employeeId) throws ResourceNotFoundException {
+		Employee employee=employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+		employeeRepository.deleteById(employeeId);
+		return ResponseEntity.ok().build();
+	}
+	
+
+}
---
+++ src/main/java/net/javaguides/springboot/crudrestfulwebservices/model/Employee.java
@@ -0,0 +1,66 @@
+package net.javaguides.springboot.crudrestfulwebservices.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")    //To convert class to JPA entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	@Column(name="firstName")
+	private String firstName;
+	@Column(name="lastName")
+	private String lastName;
+	@Column(name="emailId")
+	private String emailId;
+	
+	
+	public Employee() {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	
+	public Employee(long id, String firstName, String lastName, String emailId) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	
+
+}
